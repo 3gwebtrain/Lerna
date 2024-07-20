bc95b1dfece0c1c720213da4e4b6c85a47436872
3gwebtrain <3gwebtrain@gmail.com>
2024-07-20 22:42:51 +0530

    basic commit

---
+++ packages/doctor/src/App.tsx
@@ -0,0 +1,13 @@
+import { dummy } from "@project/components";
+import "./App.css";
+
+function App() {
+  console.log(dummy()); //calling the function
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
